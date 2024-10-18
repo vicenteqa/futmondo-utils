2b188ffb96a3eecfad784b1013e297b62f4323dc
Vicente <virucu@gmail.com>
2024-10-18 13:55:09 +0200

    add retries and sleep

--- src/endpoints/get-market.js
+++ src/endpoints/get-market.js
@@ -10,13 +10,22 @@ function setBody() {
   return body;
 }
 
-export async function getMarket() {
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function getMarket(tries = 10) {
   const body = setBody();
   try {
     const response = await postData(endpoint, body);
     if (response.answer.length > 15) return response.answer;
     else console.log(response.answer.code);
   } catch (error) {
-    console.error('Error calling market endpoint:', error);
+    if (tries > 0) {
+      tries--;
+      console.log(`Retrying ${endpoint} ${tries} more times`);
+      await sleep(2000);
+      await getMarket(tries);
+    } else console.log(`${endpoint} giving ${error.status}`);
   }
 }
