c12546df5fa7194594e3c2bfe1d89681aabadfcd
Vicente <virucu@gmail.com>
2024-10-18 19:53:50 +0200

    todos mercado

--- src/features/mejores-mercado.feat.js
+++ src/features/mejores-mercado.feat.js
@@ -5,23 +5,22 @@ export async function getPlayersWithHigherChange() {
   const market = await getMarket();
   const sortedMarket = market.sort((a, b) => b.change - a.change);
 
-  const bestOfMarket = sortedMarket
-    .filter((player) => player.change > 80000)
-    .map((player) => ({
-      jugador: player.name,
-      propietario: player.userTeam || 'Computer',
-      cambio: formatCurrency(player.change),
-      ofertas: player.numberOfBids || 0,
-      precio: formatCurrency(player.price),
-      medias: {
-        total: player.average.average,
-        media_casa: player.average.homeAverage,
-        media_fuera: player.average.awayAverage,
-        media_cinco: player.average.averageLastFive,
-        forma: JSON.stringify(player.average.fitness),
-      },
-      id: player.id,
-    }));
+  const bestOfMarket = sortedMarket.map((player) => ({
+    jugador: player.name,
+    propietario: player.userTeam || 'Computer',
+    cambio: formatCurrency(player.change),
+    ofertas: player.numberOfBids || 0,
+    precio: formatCurrency(player.price),
+    medias: {
+      total: player.average.average,
+      media_casa: player.average.homeAverage,
+      media_fuera: player.average.awayAverage,
+      media_cinco: player.average.averageLastFive,
+      forma: JSON.stringify(player.average.fitness),
+    },
+    id: player.id,
+  }));
+
   return bestOfMarket;
 }
 
