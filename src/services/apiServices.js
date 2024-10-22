0545092c61e597227b9dc1bf05998e1a49248084
Vicente <virucu@gmail.com>
2024-10-22 22:45:13 +0200

    more improvements

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: 'https://api.futmondo.com',
-  timeout: 10000,
+  timeout: 20000,
 });
 
 export async function postData(endpoint, data) {
